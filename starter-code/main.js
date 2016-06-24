b48f6a86498a550e5bb3a75a1d380a2aeac7cb0a
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-06-24 14:22:34 -0700

    ch 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,3 @@
-console.log("JS file is connected to HTML! Woo!")
-
-var cardOne = 'king'
-var cardTwo = 'king'
-var cardThree = 'queen'
-var cardFour = 'queen'
 
 
 console.log("JS file is connected to HTML! Woo!")
@@ -14,7 +8,22 @@ var cardThree = 'queen'
 var cardFour = 'queen'
 
 
-if (cardTwo === cardThree)  {
-	alert ("Sorry, try again!")
+// if (cardTwo === cardThree)  {
+// 	alert ("Sorry, try again!")
 
+// }
+
+var gameBoard = document.getElementById("game-board");
+
+
+
+var createBoard 
+
+for (createBoard = 0; createBoard < 4; createBoard++) {
+	// Inside of this function, write a loop that will run four times.
+	// Each time the loop is run, create a card element as described in step 5	
+	
+	document.createElement('div');
+	createBoard.className = "card";
 }
+
